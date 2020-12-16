77557b0f117a0b7679cbf808ee8c48762a7e18c0
I-Georg <47190936+I-Georg@users.noreply.github.com>
2020-12-16 12:14:45 +0000

    added list of items,working on create

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+
+from flask import Flask,request, render_template,redirect,url_for
+
 
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -8,7 +11,19 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    #return 'Hello World!'
+	#items = get_items()
+	
+	return render_template("index.html",items = get_items())
+	
+@app.route('/create', methods =['POST','GET'])
+def create():
+    #return 'Hello World!'
+	#items = get_items()
+	title = request.form.get('title')
+	print(title)
+	#return render_template("index.html")
+	return redirect(url_for('index'),)
 
 
 if __name__ == '__main__':
