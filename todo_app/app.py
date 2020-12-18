823177f3673ba4689a030bd98b0ded836df158d9
I-Georg <47190936+I-Georg@users.noreply.github.com>
2020-12-18 12:45:37 +0000

    commiting added as mark as completed

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,8 +8,11 @@ from todo_app.data.session_items import get_items
 from todo_app.data.session_items import add_item
 from todo_app.data.session_items import get_item
 from todo_app.data.session_items import save_item
+<<<<<<< HEAD
 =======
 >>>>>>> 77557b0... added list of items,working on create
+=======
+>>>>>>> 9e663f8... added mark as completed
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -18,12 +21,16 @@ app.config.from_object(Config)
 @app.route('/')
 def index():
 <<<<<<< HEAD
+<<<<<<< HEAD
    
 =======
     #return 'Hello World!'
 	#items = get_items()
 	
 >>>>>>> 77557b0... added list of items,working on create
+=======
+   
+>>>>>>> 9e663f8... added mark as completed
 	return render_template("index.html",items = get_items())
 	
 @app.route('/create', methods =['POST','GET'])
@@ -43,6 +50,7 @@ def update():
 	
 	    items = get_item(get_id)
 	    updated = save_item(items)
+<<<<<<< HEAD
 		#x = sorted(updated)
    
     return redirect(url_for('index'))
@@ -54,6 +62,10 @@ def update():
 	#return render_template("index.html")
 	return redirect(url_for('index'),)
 >>>>>>> 77557b0... added list of items,working on create
+=======
+   
+    return redirect(url_for('index'))
+>>>>>>> 9e663f8... added mark as completed
 
 
 if __name__ == '__main__':
