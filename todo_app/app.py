a47fb9fc1085ee91b1f37e56651684496110d569
I-Georg <47190936+I-Georg@users.noreply.github.com>
2020-12-18 13:38:47 +0000

    restoring index.html

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,15 +4,9 @@ from flask import Flask,request, render_template,redirect,url_for
 
 from todo_app.flask_config import Config
 from todo_app.data.session_items import get_items
-<<<<<<< HEAD
 from todo_app.data.session_items import add_item
 from todo_app.data.session_items import get_item
 from todo_app.data.session_items import save_item
-<<<<<<< HEAD
-=======
->>>>>>> 77557b0... added list of items,working on create
-=======
->>>>>>> 9e663f8... added mark as completed
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,22 +14,11 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-<<<<<<< HEAD
-<<<<<<< HEAD
    
-=======
-    #return 'Hello World!'
-	#items = get_items()
-	
->>>>>>> 77557b0... added list of items,working on create
-=======
-   
->>>>>>> 9e663f8... added mark as completed
 	return render_template("index.html",items = get_items())
 	
 @app.route('/create', methods =['POST','GET'])
 def create():
-<<<<<<< HEAD
    
 	title = request.form.get('title')
 	added_item	= add_item(title)
@@ -50,22 +33,9 @@ def update():
 	
 	    items = get_item(get_id)
 	    updated = save_item(items)
-<<<<<<< HEAD
 		#x = sorted(updated)
    
     return redirect(url_for('index'))
-=======
-    #return 'Hello World!'
-	#items = get_items()
-	title = request.form.get('title')
-	print(title)
-	#return render_template("index.html")
-	return redirect(url_for('index'),)
->>>>>>> 77557b0... added list of items,working on create
-=======
-   
-    return redirect(url_for('index'))
->>>>>>> 9e663f8... added mark as completed
 
 
 if __name__ == '__main__':
