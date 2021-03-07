40a24e6fdab544e99f218e75878fb818d75cb689
I-Georg <47190936+I-Georg@users.noreply.github.com>
2021-03-07 11:58:39 +0000

    viewmodel edited

--- todo_app/data/Items.py
+++ todo_app/data/Items.py
@@ -1,11 +1,35 @@
+from datetime import date
 class ViewModel:
   def __init__(self, id, name,dateLastActivity):
     self.id = id
     self.name = name
     self.dateLastActivity = dateLastActivity
 	 
-	
-  #@property
-  #def items(self):
-	 # return self._name
+  @property
+  def items(self):
+     return self._name
+
+	def show_all_done_items():
+    today = date.today()
+    if items < 5:
+    return items
+    elsif: 
+    return recent_done_items
+
+  def recent_done_items():
+    today = date.today()
+    if items.dateLastActivity == today:
+    return items
+  return items
+
+  def older_done_items():
+    today = date.today()
+    if items.dateLastActivity != today
+    return items
+  return items
+
+
+  
+
+  
 
