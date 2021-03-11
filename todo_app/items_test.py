9bb46178cbe5d80c6d220869d6d7727afadfd9f6
I-Georg <47190936+I-Georg@users.noreply.github.com>
2021-03-11 15:52:52 +0000

    updated pytests

--- todo_app/items_test.py
+++ todo_app/items_test.py
@@ -5,7 +5,7 @@ from flask import Flask,request, render_template,redirect,url_for
 import pytest
 import requests
 import os
-
+from todo_app.data.ToDo import ToDo
 
 toDoId = os.environ['TOID']
 doingId = os.environ['DOINGID']
@@ -48,7 +48,7 @@ def test_getting_todo():
 	
 	for listNumber in range(number):
 	
-		my_objects.append(ViewModel(jsonResponse[listNumber]['id'],jsonResponse[listNumber]['name']))
+		my_objects.append(ToDo(jsonResponse[listNumber]['id'],jsonResponse[listNumber]['name'],jsonResponse[listNumber]['dateLastActivity']))
 		
 	assert my_objects[0].name == 'title'
 	assert my_objects[1].name == 'title'
@@ -68,7 +68,7 @@ def test_getting_doing():
 	doing_objects =[]
 	
 	for listNumberDoing in range(numberTwo):
-		doing_objects.append(ViewModel(jsonResponseDoing[listNumberDoing]['id'],jsonResponseDoing[listNumberDoing]['name']))
+		doing_objects.append(ToDo(jsonResponseDoing[listNumberDoing]['id'],jsonResponseDoing[listNumberDoing]['name'],jsonResponseDoing[listNumberDoing]['dateLastActivity']))
 		
 	assert doing_objects[0].name == '✋🏿 Move anything that is actually started here'
 	assert doing_objects[1].name == 'Doing'
@@ -82,8 +82,8 @@ def test_getting_done():
 	done_objects = []
 	for listNumberDone in range(numberThree):
 	
-		done_objects.append(ViewModel(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name']))	 
-	assert done_objects[0].name == '✋🏿 Move anything from doing to done here'
+		done_objects.append(ToDo(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name'],jsonResponseDone[listNumberDone]['dateLastActivity']) )
+	assert done_objects[0].name == '✋🏿 Move anything from doing to done here!'
 	assert done_objects[2].name == 'title'
 	assert done_objects[3].name == '✋🏿 Move anything ready here'
 	assert done_objects[4].name == 'titlewwwwww'
@@ -98,16 +98,15 @@ def test_show_all_done_items():
 	done_objects = []
 	today = date.today()
 	for listNumberDone in range(numberThree):
-		done_objects.append(ViewModel(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name']))
-	if numberThree < 5:	
-		 done_objects
-	
-	assert done_objects[3].name == ''
-	elif numberThree > 5: 
-		getlastactivity(id) == today
-		for listNumberDone in range(numberThree):
-			displayresponse.append(ViewModel(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name']))
+		done_objects.append(ToDo(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name'],jsonResponseDone[listNumberDone]['dateLastActivity']))
+		if numberThree < 5:	
+			return done_objects
+	assert done_objects[1].name == 'Inspiration for a Card 📝'
+	#elif numberThree > 5: 
+		#getlastactivity(id) == today
+		#for listNumberDone in range(numberThree):
+			#displayresponse.append(ToDo(jsonResponseDone[listNumberDone]['id'],jsonResponseDone[listNumberDone]['name']))
 		
-	assert displayresponse[5] == ''
+	#assert displayresponse[5] == ''
 		
 	
