337984d08f21e30da1d1e71439ee6800b5beeada
I-Georg <47190936+I-Georg@users.noreply.github.com>
2021-03-14 09:37:24 +0000

    integration_tests added assert response

--- todo_app/integration_test.py
+++ todo_app/integration_test.py
@@ -4,8 +4,6 @@ from dotenv import load_dotenv
 import os
 from os.path import join, dirname
 from dotenv import find_dotenv
-#from todo_app import create_app
-#from flask import create_app
 from todo_app.app import create_app
 from unittest.mock import patch
 import responses
@@ -15,7 +13,6 @@ import requests
 def client():
 # Use our test integration config instead of the'real' version
     file_path = find_dotenv('.env.test')
-    print(file_path)
     load_dotenv(file_path, override=True)
 # Create the new app.
 #test_app = app.create_app()
@@ -23,28 +20,14 @@ def client():
 # Use the app to create a test_client that can be used in our tests.
     with test_app.test_client() as client:
         yield client
-#def test_client():
-#    flask_app = create_app()
-# 
-#    # Flask provides a way to test your application by exposing the Werkzeug test Client
-#    # and handling the context locals for you.
-#    testing_client = flask_app.test_client()
-# 
-#    # Establish an application context before running the tests.
-#    ctx = flask_app.app_context()
-#    ctx.push()
-# 
-#    yield testing_client  # this is where the testing happens!
- 
-  #  ctx.pop()
-#def test_index_page(client):
-    #response = client.get('/')
+
 
 @patch('requests.get')
 def test_index_page(mock_get_requests, client):
 # Replace call to requests.get(url) with our own function
     mock_get_requests.side_effect = mock_get_lists
-    url = f'https://api.trello.com/1/boards/6005828032dafa5707bf5dc3/lists'
+    test_board_id = '6052828032dafa5707bf5reg'
+    url = f'https://api.trello.com/1/boards/{test_board_id}/lists'
     response = client.get(url)
     
     
@@ -58,6 +41,7 @@ def mock_get_lists(url, params):
         assert response.json() == { "id": "431228032dafa5707bf5de1",
         "dateLastActivity": "2021-03-05T15:04:16.124Z",
         "name": "test data"}
+        assert response.status_code == 200
       
         return response
     return None
