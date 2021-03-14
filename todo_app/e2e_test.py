498da8e2181e168e33fc275ef4a9b9a9e333a11f
I-Georg <47190936+I-Georg@users.noreply.github.com>
2021-03-14 12:06:19 +0000

    reworked e2e_test,added create new_item selenium test

--- todo_app/e2e_test.py
+++ todo_app/e2e_test.py
@@ -7,11 +7,19 @@ from selenium import webdriver
 #os.path.dirname(r'todo_app\app.py')
 from todo_app.app import create_app
 
+
+@pytest.fixture(scope='module')
+def create_trello_board():
+    board_id = create_trello_board()
+    os.environ['TOID'] = board_id
+    application = create_app()
+    return application
+    
 @pytest.fixture(scope='module')
 def app_with_temp_board():
 # Create the new board & update the board id environment variable
-    #board_id = create_trello_board()
-    #os.environ['TOID'] = board_id
+   # board_id = create_trello_board()
+   # os.environ['TOID'] = board_id
 # construct the new application
     application = create_app()
 # start the app in its own thread.
@@ -19,10 +27,10 @@ def app_with_temp_board():
     application.run(use_reloader=False))
     thread.daemon = True
     thread.start()
-    yield app
+    yield application
 # Tear Down
     thread.join(1)
-    delete_trello_board(board_id)
+    #delete_trello_board(board_id)
 
 @pytest.fixture(scope="module")
 def driver():
@@ -34,3 +42,11 @@ def driver():
 def test_task_journey(driver, app_with_temp_board):
     driver.get('http://localhost:5000/')
     assert driver.title == 'To-Do App'
+
+def test_create_new_item(driver, app_with_temp_board):
+     driver.get('http://localhost:5000/')
+     create_test_task_input = '//*[@id="title"]'
+     create_test_task_submit = '/html/body/p[20]/input[2]'
+     driver.find_element_by_xpath(create_test_task_input).click()
+     driver.find_element_by_xpath(create_test_task_input).send_keys("testTASk")
+     driver.find_element_by_xpath(create_test_task_submit).click()
