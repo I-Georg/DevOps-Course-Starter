743983ae98fa07b8a60c4f0bc5e3017ca13f4e5c
I-Georg <47190936+I-Georg@users.noreply.github.com>
2021-12-09 15:38:11 +0000

    clean up

--- todo_app/app.py
+++ todo_app/app.py
@@ -15,7 +15,6 @@ from flask_login import LoginManager
 from oauthlib.oauth2 import WebApplicationClient
 from flask_login import login_required
 from urllib import parse
-# import urllib.request
 from flask import request
 from flask_login import UserMixin, login_user, current_user
 from todo_app.data.UserClass import User
@@ -29,25 +28,32 @@ def create_app():
     doingId = os.environ['DOINGID']
     doneId = os.environ['DONE']
     dbconnect = os.environ['CLIENT']
+    githubId = os.environ['GITHUBID']
+    webClient = os.environ['WEBAPPLICATIONCLIENT']
+    clientSecret = os.environ['CLIENTSECRET']
+    todoBoard = os.environ['TODOBOARD']
+    doingBoard = os.environ['DOINGBOARD']
+    doneBoard = os.environ['DONEBOARD']
     app.secret_key = os.getenv('SECRET_KEY')
 
     def connectDb():
         client = pymongo.MongoClient(
             dbconnect, ssl=True, ssl_cert_reqs='CERT_NONE')
-        print(client.list_database_names())
+        # print(client.list_database_names())
         database = client["01"]
         trello_collection = database["trello_collection"]
         todo = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc3'}, {"name": 1})
+            {'idBoard': todoBoard}, {"name": 1})
         for x in todo:
             print(x)
-            x
+
         doing = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc7'}, {"name": 1})
+            {'idBoard': doingBoard}, {"name": 1})
         for x in todo:
             print(x)
+
         done = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc5'}, {"name": 1})
+            {'idBoard': doneBoard}, {"name": 1})
         for x in doing:
             print(x)
 
@@ -55,60 +61,25 @@ def create_app():
 
     @login_manager.unauthorized_handler
     def unauthenticated():
-        client = WebApplicationClient('89647e3bf34e5c0f2e50')
+        client = WebApplicationClient(webClient)
         full_redirect_url = client.prepare_request_uri(
             'https://github.com/login/oauth/authorize', redirect_uri='http://localhost:5000/callback')
-        # x = request.args.get("code")
-        # print(x)
-        # for resp in request.args.get:
-        #     print(resp.url)
-
-        # print(request.url)
-        # print(request.args.getlist)
-        # redirect_uri = 'http://localhost:5000/callback?code=df0e4d5d057d32a13d4f'
-        # code = client.parse_request_uri_response(full_redirect_url)
-        # code = parse.urlsplit(full_redirect_url)
-        # x = requests.head(full_redirect_url)
-        # code = dict(parse.parse_qsl(parse.urlsplit(full_redirect_url).query))
-        # print("AAAAAA")
-        # print(x.url)
-        # for resp in x.history:
-        #     print(resp.status_code, resp.url)
-        # res = urllib.request.urlopen(
-        #     "https://github.com/login/oauth/authorize")
-        # finalurl = res.geturl()
-        # print(finalurl)
-        # data = requests.request(
-        #     "GET", "http://localhost:5000/callback")
-        # url = data.url
-        # print(url)
-        # url_get = request.host_url
-       # current_url = request.query_string
-
-       # print(current_url)
-        # print(code["redirect_uri"])
 
         return redirect(full_redirect_url)
 
     @login_manager.user_loader
-    def load_user(user_id):
-        # client = WebApplicationClient('89647e3bf34e5c0f2e50')
-        # code = request.args.get("code")
-        # client_secret = '50d6114064c16235db5973535c97b5d6cda6faaf'
-        # (url, headers, body) = client.prepare_token_request(
-        #     'https://github.com/login/oauth/access_token', code=code, client_secret=client_secret)
-        # get_token_request = requests.post(url, headers=headers, data=body)
-        # parse_body = client.parse_request_body_response(get_token_request.text)
-        # (url, headers, body) = client.add_token("https://api.github.com/user")
-        # user = requests.get(url, headers=headers, data=body)
-        # login_user(user, remember=False, duration=None,
-        #            force=False, fresh=True)
-        user_id = User(id, role="reader")
-        return user_id
+    def load_user(id):
+
+        if id == githubId:
+            role = "writer"
+        else:
+            role = "reader"
+        id = User(id, role)
+        print(role)
+
+        return id
 
     login_manager.init_app(app)
-
-    # def callback_route():
 
     def create_items(name):
         client = pymongo.MongoClient(
@@ -116,7 +87,7 @@ def create_app():
         database = client["01"]
         date_now = datetime.now().strftime('%Y-%m-%d')
 
-        post = {"name": name, "idBoard": "6005828032dafa5707bf5dc3",
+        post = {"name": name, "idBoard": todoBoard,
                 "last_modified": date_now}
 
         trello_collection = database["trello_collection"]
@@ -130,7 +101,7 @@ def create_app():
         trello_collection = database["trello_collection"]
         date_now = datetime.now().strftime('%Y-%m-%d')
         result = trello_collection.update_one(
-            post, {"$set": {"idBoard": "6005828032dafa5707bf5dc5",
+            post, {"$set": {"idBoard": doneBoard,
                             "last_modified": date_now}}
         )
 
@@ -142,62 +113,47 @@ def create_app():
         trello_collection = database["trello_collection"]
         date_now = datetime.now().strftime('%Y-%m-%d')
         result = trello_collection.update_one(
-            post, {"$set": {"idBoard": "6005828032dafa5707bf5dc3",
+            post, {"$set": {"idBoard": todoBoard,
                             "last_modified": date_now}}
         )
-
-    # def show_done():
-    #    client = pymongo.MongoClient(
-    #        "mongodb+srv://admin:MongoAdmin1@cluster0.qtpde.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", ssl=True, ssl_cert_reqs='CERT_NONE')
-    #    database = client["01"]
-    #    trello_collection = database["trello_collection"]
-    #    date_now = datetime.now().strftime('%Y-%m-%d')
-    #    result = trello_collection.find(
-    #        {"idBoard": "6005828032dafa5707bf5dc5",
-    #         "last_modified": date_now}
-    #    )
 
     @app.route('/')
     @login_required
     def index():
         connectDb()
-        # show_done()
         client = pymongo.MongoClient(
             dbconnect, ssl=True, ssl_cert_reqs='CERT_NONE')
         database = client["01"]
         trello_collection = database["trello_collection"]
         todo = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc3'}, {"name": 1})
+            {'idBoard': todoBoard}, {"name": 1})
         doing = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc7'}, {"name": 1})
+            {'idBoard': doingBoard}, {"name": 1})
         done = trello_collection.find(
-            {'idBoard': '6005828032dafa5707bf5dc5'}, {"name": 1})
+            {'idBoard': doneBoard}, {"name": 1})
 
         my_items = []
         doing_objects = []
         done_objects = []
 
-        for todo in trello_collection.find({'idBoard': '6005828032dafa5707bf5dc3'}):
+        for todo in trello_collection.find({'idBoard': todoBoard}):
             my_items.append(ToDo.from_mongo_db_entry(todo))
-        for doing in trello_collection.find({'idBoard': '6005828032dafa5707bf5dc7'}):
+        for doing in trello_collection.find({'idBoard': doingBoard}):
             doing_objects.append(ToDo.from_mongo_db_entry(doing))
-        for done in trello_collection.find({'idBoard': '6005828032dafa5707bf5dc5'}):
+        for done in trello_collection.find({'idBoard': doneBoard}):
             done_objects.append(ToDo.from_mongo_db_entry(done))
         view_model = ViewModel(my_items, doing_objects, done_objects)
-        # todo_item = ToDo.from_mongo_db_entry(todo)
         view_model.show_all_done_items()
         user = current_user.role
 
-        return render_template("index.html", my_items=my_items, doing_objects=doing_objects, done_objects=done_objects, view_model=view_model, current_user=current_user)
+        return render_template("index.html", my_items=my_items, doing_objects=doing_objects, done_objects=done_objects, view_model=view_model, user=user)
 
     @app.route('/create', methods=['POST'])
     @login_required
     def create():
         print(current_user.id)
         title = request.form.get('title')
-
         create_items(title)
-        # newToDoCard(title)
 
         return redirect(url_for('index'))
 
@@ -236,9 +192,9 @@ def create_app():
 
     @app.route('/callback')
     def login_callback():
-        client = WebApplicationClient('89647e3bf34e5c0f2e50')
+        client = WebApplicationClient(webClient)
         code = request.args.get("code")
-        client_secret = '50d6114064c16235db5973535c97b5d6cda6faaf'
+        client_secret = clientSecret
         (url, headers, body) = client.prepare_token_request(
             'https://github.com/login/oauth/access_token', code=code, client_secret=client_secret)
         get_token_request = requests.post(url, headers=headers, data=body)
@@ -249,14 +205,13 @@ def create_app():
 
         json_data = json.loads(request_text)
         id = json_data["id"]
-        if id == "I-Georg":
+        if id == githubId:
             role = "writer"
         else:
             role = "reader"
         user = User(id, role)
         login_user(user, remember=False, duration=None,
                    force=False, fresh=True)
-        # load_user(user)
 
         return redirect(url_for('index'))
 
