20952eb86abea99673f927de63aa7144744da3ed
I-Georg <47190936+I-Georg@users.noreply.github.com>
2022-02-07 09:55:46 +0000

    main.tf updated

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "webapp_url" {
+value = "https:/https://illigeorgieva102.azurewebsites.net/"
+}
+
+output"cd_webhook" {
+value= "https://Ilina.Georgieva@softwireacademy.onmicrosoft.com:Password@$Kurs1@illigeorgieva102.scm.azurewebsites.net/docker/hook"
+}
